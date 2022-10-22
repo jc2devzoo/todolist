9f647d285c90cf509e4a1954fc97596a558a7a19
profileprobono <juliohosp@hotmail.com>
2022-10-22 14:35:01 -0400

    att steps

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,28 @@
 
     <img class="img-responsive" src="@/assets/lista.png" alt="CoffeTagTodo">
 
+    <form>
+      <div class="input-group">
+          <input type="text" class="form-input" placeholder="adicionar uma tarefa">
+          <button class="btn btn-success input-group-btn"> adicionar <i class="icon icon-arrow-right"></i></button>
+      </div>
+    </form>
+
+    <ul class="step">
+      <li class="step-item active">
+        <a href="#" class="tooltip" data-tooltip="Step 1">Step 1</a>
+        </li>
+      <li class="step-item ">
+        <a href="#" class="tooltip" data-tooltip="Step 2">Step 2</a>
+      </li>
+      <li class="step-item">
+        <a href="#" class="tooltip" data-tooltip="Step 3">Step 3</a>
+      </li>
+      <li class="step-item ">
+        <a href="#" class="tooltip" data-tooltip="Step 4">Step 4</a>
+      </li>
+  </ul>
+
   </div>
 </div>
  
@@ -23,5 +45,9 @@ export default {
   img {
     max-width: 90px;
     margin:0 auto;
+  }
+
+  .input-group{
+    margin-top: 3vh;
   }
 </style>
