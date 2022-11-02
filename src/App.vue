bfd34fba80be10fd791ed20bd915a70de00b5748
profileprobono <juliohosp@hotmail.com>
2022-11-02 16:12:20 -0400

    components

--- src/App.vue
+++ src/App.vue
@@ -9,35 +9,48 @@
           <input type="text" v-model="tarefa.description" class="form-input" placeholder="adicionar uma tarefa">
           <button class="btn btn-success input-group-btn"> adicionar <i class="icon icon-arrow-right"></i></button>
       </div>
+
+      <ul class="step">
+        <li class="step-item active">
+          <a href="#" class="tooltip" data-tooltip="Step 1">Step 1</a>
+        </li>
+        <li class="step-item ">
+          <a href="#" class="tooltip" data-tooltip="Step 2">Step 2</a>
+        </li>
+        <li class="step-item">
+          <a href="#" class="tooltip" data-tooltip="Step 3">Step 3</a>
+        </li>
+        <li class="step-item ">
+          <a href="#" class="tooltip" data-tooltip="Step 4">Step 4</a>
+        </li>
+      </ul>
+
+      {{tarefas}}
+
     </form>
 
-    <ul class="step">
-      <li class="step-item active">
-        <a href="#" class="tooltip" data-tooltip="Step 1">Step 1</a>
-        </li>
-      <li class="step-item ">
-        <a href="#" class="tooltip" data-tooltip="Step 2">Step 2</a>
-      </li>
-      <li class="step-item">
-        <a href="#" class="tooltip" data-tooltip="Step 3">Step 3</a>
-      </li>
-      <li class="step-item ">
-        <a href="#" class="tooltip" data-tooltip="Step 4">Step 4</a>
-      </li>
-  </ul>
+    
 
   </div>
 </div>
  
+
   
 </template>
 
 <script>
 
+  import Tarefa from './components/Tarefa'
 
-export default {
-  name: 'App',
+  export default {
 
+  name:'App',
+  components:{
+
+    Tarefa
+    
+  },
+    
   data(){
     return {tarefas: [], tarefa: {checked: false}};
   },
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="title flex-centured ">
+         <dir class="title-content">
+            <div class="title-subtitle"> {{tarefa.description}}</div>
+         </dir>
+    </div>
+</template>
+<script>
+import { dir } from 'console';
+
+
+    export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Tarefa",
+    components: { dir }
+}
+
+</script>
+<style scoped>
+
+</style>
